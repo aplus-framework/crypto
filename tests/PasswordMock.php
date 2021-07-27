d765b8f82bf703cfab9ca8631d496cd9cc09d4c3
Natan Felles <natanfelles@gmail.com>
2021-07-27 12:47:48 -0300

    Add header comment on PasswordMock

--- tests/PasswordMock.php
+++ tests/PasswordMock.php
@@ -1,4 +1,13 @@
-<?php namespace Tests\Crypto;
+<?php
+/*
+ * This file is part of Aplus Framework Crypto Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tests\Crypto;
 
 use Framework\Crypto\Password;
 
